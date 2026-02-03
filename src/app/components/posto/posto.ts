d293880cbe4817d9cd729cb337d1226f9e835b1a
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2026-02-03 20:37:10 -0300

    criando a estrutura do postoService e base do form

--- src/app/components/posto/posto.ts
+++ src/app/components/posto/posto.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { PostoInterface } from '../../interfaces/PostoInterface';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-posto',
@@ -6,6 +8,31 @@ import { Component } from '@angular/core';
   templateUrl: './posto.html',
   styleUrl: './posto.css',
 })
-export class Posto {
+export class Posto implements OnChanges {
+  // Construindo objetos vazios
+  @Input()
+  posto: PostoInterface = {} as PostoInterface;
 
+  @Input()
+  postos: PostoInterface[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+  }
+
+  formGroupPosto: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroupPosto = formBuilder.group({
+      id: { value: null, disabled: true },
+      name: ['']
+    })
+  };
+
+  save() {
+
+  }
+
+  cancel() {
+    
+  }
 }
--- src/app/services/posto-service.ts
+++ src/app/services/posto-service.ts
@@ -11,4 +11,16 @@ export class PostoService {
   getPostos() {
     return this.http.get<PostoInterface[]>("http://localhost:8080/postos");
   }
+
+  save(posto: PostoInterface) {
+    return this.http.post<PostoInterface>("http://localhost:8080/postos", posto);
+  }
+
+  update(posto: PostoInterface) {
+    return this.http.put<PostoInterface>(`http://localhost:8080/postos/${posto.id}`, posto);
+  }
+
+  delete(posto: PostoInterface) {
+    return this.http.delete<PostoInterface>(`http://localhost:8080/postos/${posto.id}`);
+  }
 }
